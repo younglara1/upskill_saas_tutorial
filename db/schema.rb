a8ffb955558d4a1b19c7a99b076ed087f3d83baf
Lara Young <younglara1@gmail.com>
2020-05-04 17:57:57 +0000

    Added create action to save to db

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
- # This file is auto-generated from the current state of the database. Instead
+# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
@@ -13,6 +13,11 @@
 ActiveRecord::Schema.define(version: 20200430164038) do
 
   create_table "contacts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "comments"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
